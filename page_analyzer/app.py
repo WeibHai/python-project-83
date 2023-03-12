648e4da4f4b8ce2b98da1e7c917ea1aec59a6e10
марк проскурин <proskurina-1956@mail.ru>
2023-03-12 17:39:26 +0300

    new func

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request
 from flask import flash, url_for, redirect, get_flashed_messages
-from urllib.parse import urlparse
-from page_analyzer.validator import validate
+from flask import Flask, render_template, request
 from page_analyzer.connector import send_in_db
-from datetime import date
-import os
+from page_analyzer.validator import validate
+from urllib.parse import urlparse
 from dotenv import load_dotenv
-
+from datetime import date
+import requests
+import os
 
 
 app = Flask(__name__)
@@ -25,22 +25,24 @@ def analyzer():
 
 @app.get('/urls')
 def urls():
-    query = "SELECT * FROM urls ORDER BY created_at DESC"
+    query_urls = "SELECT DISTINCT * FROM urls, url_checks"
 
-    response = send_in_db(query)
+    response_urls = send_in_db(query_urls)
 
-    return render_template('list_urls_page.html', list_urls=response)
+    return render_template('list_urls_page.html', site=response_urls)
 
 
-@app.get('/urls/<id>')
+@app.route('/urls/<id>', methods=['GET', 'POST'])
 def url(id):
-    #messages = get_flashed_messages(with_categories=True)
+    messages = get_flashed_messages(with_categories=True)
 
-    query = f"SELECT * FROM urls WHERE id='{id}'"
+    query_urls = f"SELECT * FROM urls WHERE id='{id}'"
+    query_url_checks = f"SELECT * FROM url_checks WHERE url_id='{id}'"
 
-    response = send_in_db(query)
+    response_urls = send_in_db(query_urls)
+    response_url_checks = send_in_db(query_url_checks, 'one')
 
-    return render_template('url_page.html', site=response)
+    return render_template('url_page.html', messages=messages,  site=response_urls, checklist=response_url_checks, site_id=id, site_url=response_urls[0][1])
 
 
 @app.post('/urls')
@@ -58,10 +60,38 @@ def post_analyzer():
 
         return redirect(url_for('analyzer'))
 
-    query = f'''INSERT INTO urls (name, created_at)
+    query_insert = f'''INSERT INTO urls (name, created_at)
                 VALUES ('{netloc}', '{date.today()}')'''
 
-    send_in_db(query)
+    send_in_db(query_insert)
+
+    query_id = f'''SELECT MAX(id) FROM urls'''
+
+    id = send_in_db(query_id, 'one')
 
     flash('Страница успешно добавлена', 'access')
-    return redirect(url_for('urls'))
+    return redirect(url_for('url', id=id[0]))
+
+
+@app.post('/urls/<id>/checks')
+def post_checks(id):
+    query_id = f'''SELECT name FROM urls WHERE id={id}'''
+
+    url = send_in_db(query_id)
+
+    urlssss = url[0][0]
+
+    try:
+        checks_requests = requests.get('https://' + urlssss)
+
+    except Exception as _ex:
+        flash('Произошла ошибка при проверке', 'error')
+        return redirect(url_for('url', id=id))
+    
+    query_insert = f'''INSERT INTO url_checks (url_id, status_code, created_at)
+                           VALUES ('{id}', '{checks_requests.status_code}', '{date.today()}')'''
+
+    send_in_db(query_insert)
+
+    flash('Страница успешно проверенна', 'access')
+    return redirect(url_for('url', id=id))
