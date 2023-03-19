6b7fbc6bfcf73b78ef586c953e1121dd66691b02
марк проскурин <proskurina-1956@mail.ru>
2023-03-19 21:00:34 +0300

    fix bugs

---
+++ database.sql
@@ -0,0 +1,19 @@
+createdb  test_pg_analyzer
+createdb  pg_analyzer
+
+CREATE TABLE urls (
+    id              bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name            varchar(255) UNIQUE,
+    created_at      date
+);
+
+
+CREATE TABLE url_checks (
+    id              bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id          bigint REFERENCES urls (id),
+    status_code     smallint,
+    h1              varchar(255),
+    title           varchar(255),
+    description     varchar(255),
+    created_at      date
+);
