b873518c8319ae0b297a229d129eee11d4a868db
марк проскурин <proskurina-1956@mail.ru>
2023-03-25 13:06:06 +0300

    deploy

--- database.sql
+++ database.sql
@@ -1,17 +1,13 @@
-DROP TABLE if exists urls CASCADE;
-DROP TABLE if exists url_checks CASCADE;
-
-
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id              bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name            varchar(255) UNIQUE NOT NULL,
     created_at      date NOT NULL
 );
 
 
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS  url_checks (
     id              bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id          bigint NOT NULL REFERENCES urls (id),
+    url_id          bigint NOT NULL REFERENCES urls (id) ON DELETE CASCADE,
     status_code     smallint NOT NULL,
     h1              varchar(255),
     title           varchar(255),
