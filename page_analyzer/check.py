56307a7c84c6ef17cff41f776c60e22372a6ece4
Марк Проскурин <103996243+WeibHai@users.noreply.github.com>
2023-04-04 19:49:19 +0300

    fix bugs

--- page_analyzer/check.py
+++ page_analyzer/check.py
@@ -24,6 +24,6 @@ def get_check(url):
         else:
             raise ('No connection to site')
 
-    except Exception as _ex:
+    except Exceptionx:
         logging.error('Error during check!')
         return {}
