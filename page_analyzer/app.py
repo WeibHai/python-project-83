b827fbd3fc2be274c600d392e1fd2f388b998111
Марк Проскурин <103996243+WeibHai@users.noreply.github.com>
2023-04-05 18:39:41 +0300

    Update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 from flask import flash, url_for, redirect, make_response
 from flask import Flask, render_template, request
 from page_analyzer.validator import validate
-from page_analyzer.parser import normalize_url
+from page_analyzer.normalizator import normalize_url
 from page_analyzer.db import get_one_from_db
 from page_analyzer.db import get_all_from_db
 from page_analyzer.db import find_in_db
