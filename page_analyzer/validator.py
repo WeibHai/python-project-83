fd83463121d4cba26f507405052aed690dae4753
Марк Проскурин <103996243+WeibHai@users.noreply.github.com>
2023-04-05 18:07:39 +0300

    Update validator.py

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -7,7 +7,7 @@ def validate(url):
     if not url:
         errors.append('URL обязателен')
         return errors
-    
+
     else:
         if not validators.url(url):
             errors.append('Некорректный URL')
