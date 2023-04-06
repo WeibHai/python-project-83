1c6dce41b053d7585e3ab1b24cdd125e88b97b5c
Марк Проскурин <103996243+WeibHai@users.noreply.github.com>
2023-04-06 18:20:47 +0300

    Update check.py

--- page_analyzer/check.py
+++ page_analyzer/check.py
@@ -7,23 +7,12 @@ import requests
 
 
 def get_check(url):
-    try:
-        response = requests.get(url)
+    result = {'status_code': response.status_code}
 
-        if response.status_code == 200:
-            result = {'status_code': response.status_code}
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-            soup = BeautifulSoup(response.text, 'html.parser')
+    result['description'] = get_description(soup)
+    result['title'] = get_title(soup)
+    result['h1'] = get_h1(soup)
 
-            result['description'] = get_description(soup)
-            result['title'] = get_title(soup)
-            result['h1'] = get_h1(soup)
-
-            return result
-
-        else:
-            raise ('No connection to site')
-
-    except Exception:
-        logging.error('Error during check!')
-        return {}
+    return result
