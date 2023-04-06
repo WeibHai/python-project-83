71db681972d2f7d78be3c773758495b91465c34c
Марк Проскурин <103996243+WeibHai@users.noreply.github.com>
2023-04-06 17:59:12 +0300

    Update normalizator.py

--- page_analyzer/normalizator.py
+++ page_analyzer/normalizator.py
@@ -1,4 +1,12 @@
 from urllib.parse import urlparse
+from urllib.parse import 
+
+
+def normalize_url(url):
+    raw_result = urlparse(url)
+    result = raw_result._replace(path='', params='', query='', fragment='')
+
+    return result.geturl()
 
 
 def normalize_url(url):
